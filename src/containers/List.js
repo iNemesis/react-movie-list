0dc7aa28d4529c1ebd6c9e711d108f84e0e2a765
Antoine MAZURE <antoine.mazure@numen.fr>
2020-10-31 20:55:11 +0100

    :sparkles: use hooks and func components

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,44 +1,33 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card/Card';
 
-class List extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: [],
-            loading: true,
-        };
-    }
+function List () {
+    let [movies, setMovies] = useState([]);
+    let [isLoading, setIsLoading] = useState(true);
 
-    async componentDidMount() {
-        const movies = await fetch("src/assets/data.json");
-        const moviesJSON = await movies.json();
-
+    useEffect(async () => {
+        const moviesRaw = await fetch("src/assets/data.json");
+        const moviesJSON = await moviesRaw.json();
+    
         if (moviesJSON) {
-            this.setState({
-                data: moviesJSON,
-                loading: false,
-            });
+            setMovies(moviesJSON);
+            setIsLoading(false);
         }
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>
     }
 
-    render() {
-        const { data, loading } = this.state;
-
-        if (loading) {
-            return <div>Loading...</div>
-        }
-
-        return (
-            <div className='row'>
-                {data.map(movie =>
-                    <div key={ movie.id } className='col-sm-2'>
-                        <Card movie={ movie } />
-                    </div>    
-                )}
-            </div>
-        );
-    }
+    return (
+        <div className='row'>
+            {movies.map(movie =>
+                <div key={ movie.id } className='col-sm-2'>
+                    <Card movie={ movie } />
+                </div>    
+            )}
+        </div>
+    );
 }
 
 export default List;
